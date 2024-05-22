08e7fa71e461077944bbe1f4d7c826bc1a282005
grzegorzolszewskii <yapinion@gmail.com>
2024-05-22 19:23:54 +0200

    Service + ServiceImpl

--- wsb-jpa1/src/main/java/com/capgemini/wsb/service/PatientService.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/service/PatientService.java
@@ -1,8 +1,15 @@
 package com.capgemini.wsb.service;
 
 import com.capgemini.wsb.dto.PatientTO;
+import com.capgemini.wsb.dto.VisitTO;
+
+import java.util.List;
 
 public interface PatientService
 {
     public PatientTO findById(final Long id);
+
+    public void deletePatient(Long id);
+
+    public List<VisitTO> findVisitsByPatientId(Long id);
 }
--- wsb-jpa1/src/main/java/com/capgemini/wsb/service/impl/PatientServiceImpl.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/service/impl/PatientServiceImpl.java
@@ -1,13 +1,18 @@
 package com.capgemini.wsb.service.impl;
 
 import com.capgemini.wsb.dto.PatientTO;
+import com.capgemini.wsb.dto.VisitTO;
 import com.capgemini.wsb.mapper.PatientMapper;
+import com.capgemini.wsb.mapper.VisitMapper;
 import com.capgemini.wsb.persistence.dao.PatientDao;
 import com.capgemini.wsb.persistence.entity.PatientEntity;
 import com.capgemini.wsb.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -26,6 +31,18 @@ public class PatientServiceImpl implements PatientService
     public PatientTO findById(Long id) {
         final PatientEntity entity = patientDao.findOne(id);
         return PatientMapper.mapToTO(entity);
+    }
+
+    @Override
+    public void deletePatient(Long id) {
+        patientDao.delete(id);
+    }
+
+    @Override
+    public List<VisitTO> findVisitsByPatientId(Long id) {
+        return patientDao.findVisitsByPatientId(id)
+                .stream().map(VisitMapper::mapToTO)
+                .collect(Collectors.toList());
     }
 }
 
